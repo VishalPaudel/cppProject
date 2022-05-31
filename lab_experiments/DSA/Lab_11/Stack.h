8b80ec40a15617962df348f317d7a9a080089745
Vishal Paudel <95016059+VishalPaudel@users.noreply.github.com>
2022-05-31 08:15:34 +0000

    _

---
+++ lab_experiments/DSA/Lab_11/Stack.h
@@ -0,0 +1,82 @@
+
+#include <iostream>
+
+
+const int MAX=100;		// The Stack Size
+
+
+template <class Type>
+class Stack {
+	
+private:
+	Type arr[MAX];
+	int top;		// indexing for the Stack
+
+public:
+	Stack() {  top = -1;  }	// Initially the Stack is set empty
+	
+	void push(Type);	// Defined outside
+	
+	Type pop();		// To POP! the fist / top element, defined outside
+
+	bool isEmpty()	{
+
+		if (top == -1) {  return true;  }
+		else {  return false;  }
+
+	}
+
+	bool isFull()	{
+		
+		if (top == MAX - 1)  {  return true;  }
+		else {  return false;  }
+
+	}
+
+	Type peek()	{
+
+		return arr[top];
+
+	}
+};
+
+
+template <class Type>
+void Stack<Type> :: push(Type var)
+{
+
+	try {
+		
+		if (top == MAX-1) throw top;
+
+		else {  arr[++top] = var;  }
+
+	}
+	catch (int x)	{
+		
+		std::cout << "Stack overflow!" << endl;
+		exit(1);
+
+	}
+}
+
+
+template <class Type>
+Type Stack<Type> :: pop()
+{
+
+	try {
+	
+		if (top == -1)  throw top;
+		else return arr[top--];
+
+	}
+	
+	catch (int x) {
+	
+		std::cout << "Stack underflow!";
+		exit(1);
+
+	}
+}
+
---
+++ lab_experiments/DSA/Lab_11/main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+
+
+int main () {
+	
+	std::cout << "Everything seems to work!!" << std::endl;
+	
+	Stack <int> S1, S2;
+
+	S1.pop();
+
+	return EXIT_SUCCESS;
+
+}
